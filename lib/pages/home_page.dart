2d81e9a3039fbff0f71d5ca91064da1c8f70f692
luidimso <luidi_mso@outlook.com>
2021-03-29 19:35:09 -0300

    Bottom navigation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,8 +6,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  PageController _pageController;
+  int _page = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.pinkAccent,
+          primaryColor: Colors.white,
+          textTheme: Theme.of(context).textTheme.copyWith(
+            caption: TextStyle(
+              color: Colors.white54
+            )
+          )
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _page,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                title: Text("Clients")
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart),
+                title: Text("Orders")
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                title: Text("Products")
+            )
+          ],
+          onTap: (page) {
+            _pageController.animateToPage(page,
+                duration: Duration(
+                  milliseconds: 500
+                ),
+                curve: Curves.ease
+            );
+          },
+        ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (page) {
+          setState(() {
+            _page = page;
+          });
+        },
+        children: <Widget>[
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.green
+          )
+        ],
+      ),
+    );
   }
 }
